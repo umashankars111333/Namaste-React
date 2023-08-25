506caf569a798393e9916cd99474bf14e0168edf
Umashankar <uumashankars111333@gmail.com>
2023-08-25 20:30:25 +0530

    Day 3 Namaste React Laying the Foundation

--- App.js
+++ App.js
@@ -1,16 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", null, "Im h1 Tag"),
-    React.createElement("h1", null, "Im h1 Tag"),
-  ]),
-  ,
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", null, "Im h2 Tag"),
-    React.createElement("h1", null, "Im h2 Tag"),
-  ]),
-]);
+// React Element
+// const title = <h1>Namaste React</h1>;
+
+const Title = () => <h1>Namaste React</h1>;
+
+const number = 2000;
+// React Function Component
+const Heading = () => {
+  return (
+    <div>
+      {number}
+      <Title />
+      <div>Episode 3 By Akshay Saini</div>
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<Heading />);
